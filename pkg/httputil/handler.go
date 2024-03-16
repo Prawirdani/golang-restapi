b527311475712fe05d9ea4ee4acf8bf14d4cf55e
Prawirdani <prawirdani@gmail.com>
2024-03-16 20:51:39 +0700

    fix mismatch module import

--- pkg/httputil/handler.go
+++ pkg/httputil/handler.go
@@ -2,8 +2,6 @@ package httputil
 
 import (
 	"net/http"
-
-	"github.com/prawirdani/golang-restapi/pkg/json"
 )
 
 type handlerFunc func(w http.ResponseWriter, r *http.Request) error
@@ -13,11 +11,11 @@ func HandlerWrapper(fn handlerFunc) http.HandlerFunc {
 		if err := fn(w, r); err != nil {
 			// If error was the object of ApiError
 			if e, ok := err.(*ApiError); ok {
-				json.Send(w, e.Status, Response{Error: e.Error()})
+				JsonSend(w, e.Status, Response{Error: e.Error()})
 				return
 			}
 			// Otherwise create an InternalServerError
-			json.Send(w, http.StatusInternalServerError, Response{Error: err.Error()})
+			JsonSend(w, http.StatusInternalServerError, Response{Error: err.Error()})
 		}
 
 	}
