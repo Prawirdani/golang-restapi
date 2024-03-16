9c4ee99b1d6dac6b13828410d0becb960832ab97
Prawirdani <prawirdani@gmail.com>
2024-03-16 07:28:57 +0700

    handlerWrapper function

---
+++ internal/common/api/handler.go
@@ -0,0 +1,24 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/prawirdani/golang-restapi/internal/common/json"
+)
+
+type handlerFunc func(w http.ResponseWriter, r *http.Request) error
+
+func HandlerWrapper(fn handlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := fn(w, r); err != nil {
+			// If error was the object of ApiError
+			if e, ok := err.(*ApiError); ok {
+				json.Send(w, e.Status, Response{Error: e.Error()})
+				return
+			}
+			// Otherwise create an InternalServerError
+			json.Send(w, http.StatusInternalServerError, Response{Error: err.Error()})
+		}
+
+	}
+}
