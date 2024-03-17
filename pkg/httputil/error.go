6f230b952098dda5e377ac6de63e1351e592fa52
Prawirdani <prawirdani@icloud.com>
2024-03-18 00:53:01 +0700

    add: error parsers

--- pkg/httputil/error.go
+++ pkg/httputil/error.go
@@ -1,31 +1,16 @@
 package httputil
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
 )
 
-type ApiError struct {
-	// Response Status Code
-	Status int
-	// Error Message
-	Message any
-}
-
-func (e *ApiError) Error() string {
-	return fmt.Sprint(e.Message)
-}
-
-func buildApiError(status int) func(e error) *ApiError {
-	return func(e error) *ApiError {
-		return &ApiError{
-			Status:  status,
-			Message: e,
-		}
-	}
-}
-
 // Use these error wrappers for known errors to have precise response status codes, can be used on any abstraction layer.
+// You can either pass string, error object or anything as the error cause message.
 var (
 	ErrBadRequest       = buildApiError(http.StatusBadRequest)
 	ErrConflict         = buildApiError(http.StatusConflict)
@@ -34,3 +19,75 @@ var (
 	ErrInternalServer   = buildApiError(http.StatusInternalServerError)
 	ErrMethodNotAllowed = buildApiError(http.StatusMethodNotAllowed)
 )
+
+type ApiError struct {
+	// Response Status Code
+	Status int
+	// Error Cause
+	Cause interface{}
+}
+
+// Return ApiErr in string format
+func (e *ApiError) Error() string {
+	return fmt.Sprintf("status: %v cause: %v", e.Status, e.Cause)
+}
+
+func buildApiError(status int) func(err interface{}) *ApiError {
+	return func(e interface{}) *ApiError {
+		return &ApiError{
+			Status: status,
+			Cause:  e,
+		}
+	}
+}
+
+// Error parser, parse every error an turn it into ApiError,
+// So it can be used to determine what status code should be put on the res headers.
+// You can always add your `known error` or make a custom parser for 3rd library/package error.
+func parseError(err error) *ApiError {
+	// By Error string
+	switch {
+	case strings.Contains(err.Error(), "EOF"): // Empty JSON Req body
+		return ErrBadRequest("Empty json request body")
+	}
+
+	// By Error type
+	switch e := err.(type) {
+	// If the error is instance of ApiErr then no need to do aditional parsing.
+	case *ApiError:
+		return e
+	case validator.ValidationErrors:
+		return parseValidationError(e)
+	case *json.UnmarshalTypeError:
+		return parseJsonUnmarshalTypeError(e)
+	default:
+		return ErrInternalServer(err.Error())
+	}
+
+}
+
+// For go-playground/validator/v10 package
+func parseValidationError(err validator.ValidationErrors) *ApiError {
+	// Validation error mapped into a map, so the response will look like "field":"the error"
+	errors := make(map[string]interface{})
+	for _, errField := range err {
+		field := strings.ToLower(errField.Field())
+		switch errField.Tag() {
+		case "required":
+			errors[field] = fmt.Sprintf("%s field is required", field)
+		case "email":
+			errors[field] = "Invalid email format"
+		default:
+			errors[field] = errField.Error()
+		}
+	}
+	return ErrBadRequest(errors)
+}
+
+// JSON Unmarshal mismatch type error
+func parseJsonUnmarshalTypeError(err *json.UnmarshalTypeError) *ApiError {
+	if strings.Contains(err.Error(), "unmarshal") {
+		return ErrBadRequest(fmt.Sprintf("Type mismatch at %s, Expected type %s, Got %s", err.Field, err.Type, err.Value))
+	}
+	return ErrBadRequest(err.Error())
+}
