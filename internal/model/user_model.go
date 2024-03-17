9e5c9feb25681bb3fc314c2bbd13155dddcd8cd5
Prawirdani <prawirdani@icloud.com>
2024-03-17 22:17:35 +0700

    add: email tag validator

--- internal/model/user_model.go
+++ internal/model/user_model.go
@@ -12,11 +12,11 @@ type UserResponse struct {
 
 type UserRegisterRequest struct {
 	Name     string `json:"name" validate:"required"`
-	Email    string `json:"email" validate:"required"`
+	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required"`
 }
 
 type UserLoginRequest struct {
-	Email    string `json:"email" validate:"required"`
+	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required"`
 }
