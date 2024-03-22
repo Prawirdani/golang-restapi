d217ddc99a9fdad7d707dc69169c6adb76413800
Prawirdani <prawirdani@icloud.com>
2024-03-22 08:25:19 +0700

    remove validator

--- internal/model/auth_model.go
+++ internal/model/auth_model.go
@@ -1,22 +1,12 @@
 package model
 
-import "github.com/prawirdani/golang-restapi/pkg/utils"
-
 type RegisterRequestPayload struct {
-	Name     string `json:"name" validate:"required"`
-	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required,min=6"`
-}
-
-func (r *RegisterRequestPayload) Validate() error {
-	return utils.ValidateStruct(r)
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
 type LoginRequestPayload struct {
-	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required"`
-}
-
-func (r *LoginRequestPayload) Validate() error {
-	return utils.ValidateStruct(r)
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
