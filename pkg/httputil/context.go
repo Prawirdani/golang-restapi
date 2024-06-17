1c288d2a5767f5dea44fea89070b43dce593d899
Prawirdani <prawirdani@gmail.com>
2024-06-17 11:26:09 +0700

    update: typed ctx key

--- pkg/httputil/context.go
+++ pkg/httputil/context.go
@@ -2,14 +2,16 @@ package httputil
 
 import "context"
 
-const TOKEN_CLAIMS_CTX_KEY = "token_claims"
+type CtxKey string
+
+const TOKEN_CLAIMS CtxKey = "token_claims"
 
 // Auth Context Setter
 func SetAuthCtx(ctx context.Context, claims map[string]interface{}) context.Context {
-	return context.WithValue(ctx, TOKEN_CLAIMS_CTX_KEY, claims)
+	return context.WithValue(ctx, TOKEN_CLAIMS, claims)
 }
 
 // Retrieve Token map claims from the Request Context
 func GetAuthCtx(ctx context.Context) map[string]interface{} {
-	return ctx.Value(TOKEN_CLAIMS_CTX_KEY).(map[string]interface{})
+	return ctx.Value(TOKEN_CLAIMS).(map[string]interface{})
 }
