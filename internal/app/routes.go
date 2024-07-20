f3708728e93d8f898f7da19d7e978a73447b7560
Prawirdani <prawirdani@gmail.com>
2024-07-21 00:29:38 +0700

    simplify CustomHandler & handler wrapper

--- internal/app/routes.go
+++ internal/app/routes.go
@@ -1,10 +1,24 @@
 package app
 
 import (
+	"net/http"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/prawirdani/golang-restapi/internal/app/middleware"
 	"github.com/prawirdani/golang-restapi/internal/handler"
+	"github.com/prawirdani/golang-restapi/pkg/httputil"
+	"github.com/prawirdani/golang-restapi/pkg/response"
 )
+
+// handlerFn is a helper function to handle error in CustomHandler function
+func handlerFn(fn httputil.CustomHandler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := fn(w, r); err != nil {
+			response.HandleError(w, err)
+			return
+		}
+	}
+}
 
 func registerAuthRoutes(r chi.Router, h *handler.AuthHandler, mw *middleware.Collection) {
 	r.Post("/auth/register", handlerFn(h.HandleRegister))
