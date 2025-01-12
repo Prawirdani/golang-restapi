e096270a84b80404da81b2e16901a333a04af0b7
Prawirdani <prawirdani@gmail.com>
2025-01-13 03:11:16 +0700

    Update: use snake case json key

--- internal/model/auth.go
+++ internal/model/auth.go
@@ -6,10 +6,10 @@ import (
 )
 
 type RegisterRequest struct {
-	Name           string `json:"name"           validate:"required,min=3"`
-	Email          string `json:"email"          validate:"required,email"`
-	Password       string `json:"password"       validate:"required,min=6"`
-	RepeatPassword string `json:"repeatPassword" validate:"required,eqfield=Password,min=6"`
+	Name           string `json:"name"            validate:"required,min=3"`
+	Email          string `json:"email"           validate:"required,email"`
+	Password       string `json:"password"        validate:"required,min=6"`
+	RepeatPassword string `json:"repeat_password" validate:"required,eqfield=Password,min=6"`
 }
 
 func (r *RegisterRequest) Validate() error {
--- pkg/validator/validator.go
+++ pkg/validator/validator.go
@@ -1,10 +1,24 @@
 package validator
 
 import (
+	"reflect"
+	"strings"
+
 	"github.com/go-playground/validator/v10"
 )
 
-var v = validator.New()
+var v *validator.Validate
+
+func init() {
+	v = validator.New()
+	v.RegisterTagNameFunc(func(field reflect.StructField) string {
+		name := strings.SplitN(field.Tag.Get("json"), ",", 2)[0]
+		if name == "-" {
+			return ""
+		}
+		return name
+	})
+}
 
 func Struct(s interface{}) error {
 	return v.Struct(s)
