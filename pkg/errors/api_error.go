b7e6ef0f599183f93e05b33ce6d8fedb3240257a
Prawirdani <prawirdani@gmail.com>
2025-01-13 03:19:37 +0700

    Add some http errors

--- pkg/errors/api_error.go
+++ pkg/errors/api_error.go
@@ -7,13 +7,18 @@ import (
 // Use these error wrappers for known errors to have precise response status codes, can be used on any abstraction layer.
 // It will only set the message in ErrorResponse, if you want to provide details in the ErrorResponse you should create ApiError object manually.
 var (
-	BadRequest       = build(http.StatusBadRequest)
-	Conflict         = build(http.StatusConflict)
-	NotFound         = build(http.StatusNotFound)
-	Unauthorized     = build(http.StatusUnauthorized)
-	MethodNotAllowed = build(http.StatusMethodNotAllowed)
-	Forbidden        = build(http.StatusForbidden)
-	InternalServer   = build(http.StatusInternalServerError)
+	BadRequest          = build(http.StatusBadRequest)          // 400
+	Unauthorized        = build(http.StatusUnauthorized)        // 401
+	Forbidden           = build(http.StatusForbidden)           // 403
+	NotFound            = build(http.StatusNotFound)            // 404
+	MethodNotAllowed    = build(http.StatusMethodNotAllowed)    // 405
+	Conflict            = build(http.StatusConflict)            // 409
+	Gone                = build(http.StatusGone)                // 410
+	UnprocessableEntity = build(http.StatusUnprocessableEntity) // 422
+	TooManyRequest      = build(http.StatusTooManyRequests)     // 429
+	InternalServer      = build(http.StatusInternalServerError) // 500
+	ServiceUnavailable  = build(http.StatusServiceUnavailable)  // 503
+	GatewayTimeout      = build(http.StatusGatewayTimeout)      // 504
 )
 
 type ApiError struct {
